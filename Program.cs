41b3298486f2edfa9ec81a31d3d1670b34ec9234
Shreya@18 <shreyantasulebhavi1gmail.com>
2022-01-15 23:18:39 +0530

    Day3 Assignment2

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Part_of_oops.Day3_Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome");
+
+           //DayUC3
+             Day3UC3 day3UC3 = new Day3UC3();
+             day3UC3.CompareLines();
+
+           //Day3UC2
+           //LinesofLength eqls = new LinesofLength();
+           // LinesofLength.uc2();
+            
+           // Day3UC1
+           //Day3UC1 day3UC1 = new Day3UC1();
+           //day3UC1.Lengthoftwopoints();
+
+           
+        }
+    }
+}
